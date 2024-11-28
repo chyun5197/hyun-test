f4b7c2f5873def93346a81d8f4ae3af64fd45005
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2024-11-28 11:26:01 +0900

    Delete hobiday-kakao directory

--- hobiday-kakao/src/main/java/com/example/hobiday_backend/domain/users/repository/ProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hobiday_backend.domain.users.repository;
-
-import com.example.hobiday_backend.domain.users.domain.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-
-}
--- hobiday-kakao/src/main/java/com/example/hobiday_backend/global/oauth/userinfo/OAuth2UserInfo.java
+++
@@ -1,6 +0,0 @@
-//package com.example.hobiday_backend.global.oauth.userinfo;//package com.culture.config.kakao;
-//
-//public interface OAuth2UserInfo {
-//    String getEmail();
-//    String getName();
-//}
