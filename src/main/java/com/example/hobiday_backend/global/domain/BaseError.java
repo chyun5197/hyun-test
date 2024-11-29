bbb0e75ed6abca214efbcdf4b9d874d3a99935fd
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2024-11-29 13:42:15 +0900

    Delete src/main/java/com/example/hobiday_backend/global directory

--- src/main/java/com/example/hobiday_backend/global/domain/BaseError.java
+++
@@ -1,9 +0,0 @@
-package com.example.hobiday_backend.global.domain;
-
-import org.springframework.http.HttpStatus;
-
-public interface BaseError {
-    HttpStatus getHttpStatus();
-    String getCode();
-    String getMessage();
-}
--- src/main/java/com/example/hobiday_backend/global/dto/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.hobiday_backend.global.dto;
-
-public enum Role {
-    ROLE_USER, // 일반 사용자
-    ROLE_ADMIN // 관리자
-}
